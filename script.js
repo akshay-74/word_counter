cf34e3f07d8dc038e6bbc6b6415eb60dc3f475cf
Papasani Akshay Kumar <akshaypapasani@gmail.com>
2025-03-11 15:02:27 +0530

    first commit

--- script.js
+++ script.js
@@ -21,12 +21,20 @@ function atLeastTwoCharacters(text) {
   const wordCountElement = document.querySelector(".word-count");
   const letterCountElement = document.querySelector(".letter-count");
   const spaceCountElement = document.querySelector(".space-count");
+  const clearButton = document.querySelector(".clear-btn");
   
   textInput.addEventListener("input", () => {
     const splitted = textInput.value.trim().split(/[\s-]/);
     const letterCount = (textInput.value.match(/[a-z]/gi) || []).length;
     const spaceCount = (textInput.value.match(/\s+/g) || []).length;
     let wordCount = 0;
+
+    clearButton.addEventListener("click", () => {
+      textInput.value = "";
+      wordCountElement.textContent = "0";
+      letterCountElement.textContent = "0";
+      spaceCountElement.textContent = "0";
+    });
   
     outer: for (const text of splitted) {
       for (const check of checks) {
